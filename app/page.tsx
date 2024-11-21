c4562bea21d75562a5de37ac57db5583045aa5f3
James <mirageapache@gmail.com>
2024-11-21 23:04:07 +0800

    [feat] 修改首頁

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,32 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <h1>首頁</h1>
-        </div>
+    <div className="">
+      <main className="">
+        <section className="flex w-screen h-screen">
+          <div className="flex items-center">
+            <div>
+              <h1>Welcome, Fire Message!</h1>
+              <p>
+                This is a simple example of a Next.js page. To get started and
+                save to see your changes.
+              </p>
+              <Image
+                className="dark:invert"
+                src="https://nextjs.org/icons/next.svg"
+                alt="Next.js logo"
+                width={180}
+                height={38}
+                priority
+              />
+              <div className="flex gap-4 items-center flex-col sm:flex-row">
+                <h1>首頁</h1>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 h-full bg-[url('/images/sectionOne.png')] bg-auto bg-center" />
+        </section>
       </main>
     </div>
   );
