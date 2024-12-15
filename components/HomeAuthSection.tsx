f39684271792adb1d1c0319fabd7b89c8ecf76a7
James <mirageapache@gmail.com>
2024-12-15 22:57:11 +0800

    [feat] 調整home page

--- components/HomeAuthSection.tsx
+++ components/HomeAuthSection.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 function HomeAuthSection() {
   return (
     <section className="flex w-screen h-screen">
-      <div className="flex items-center">
+      <div className="flex items-center bg-white z-10 bg-transparent">
         <div>
           <h1>Welcome, Fire Message!</h1>
           <p>
-            This is a simple example of a Next.js page. To get started and
-            save to see your changes.
+            This is a simple example of a Next.js page. To get started and save
+            to see your changes.
           </p>
-          <Image
-            className="dark:invert"
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <h1>首頁</h1>
-          </div>
+          {/* <Image
+          className="dark:invert"
+          src="/icons/fire_icon.png"
+          alt="logo"
+          width={60}
+          height={60}
+          priority
+        />
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <h1>首頁</h1>
+        </div> */}
+        </div>
+        <div>
+          <p>註冊&登入</p>
         </div>
       </div>
-      <div className="w-1/2 h-full bg-[url('/images/sectionOne.png')] bg-auto bg-center" />
+      <div className="fixed w-full h-screen bg-[url('/images/bg_image.png')] bg-cover bg-center opacity-50" />
     </section>
   );
 }
