7b3b1386f4cba60cda4289e6c7417d918321d83c
James <mirageapache@gmail.com>
2025-01-13 23:11:30 +0800

    [feat] 調整dashboard版面

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 function Dashboard() {
   return (
     <div className="flex w-full h-full">
-      <aside className="w-full md:max-w-80 bg-gray-700 rounded-lg p-5">
+      <aside className="hidden md:block w-full md:max-w-60 lg:max-w-80 bg-gray-700 rounded-lg p-5">
         <ChatList />
       </aside>
-      <aside className="w-full">Chat Room</aside>
+      <section className="block w-full border border-red-500 bg-gray-200">main Dashboard</section>
     </div>
   );
 }
