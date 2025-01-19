4a679b169d2d6591bd895d9fd761c1c577d8bb4b
James <mirageapache@gmail.com>
2025-01-19 20:33:08 +0800

    [optimize] 優化註冊流程

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,8 +10,16 @@ import {
   UserCredential,
 } from "firebase/auth";
 import { get } from "lodash";
-import { doc, getDoc, setDoc, collection, query, where, getDocs } from "firebase/firestore";
-import Cookies from 'universal-cookie';
+import {
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import Cookies from "universal-cookie";
 import { store } from "@/store";
 import { clearUser, setUser } from "@/store/authSlice";
 import { userDataType } from "@/types/userType";
@@ -21,37 +29,47 @@ import { auth, db } from "../firebase";
 const cookies = new Cookies();
 
 /** write new User to firestore */
-const writeUser = async (user: User, username: string | null, source: string) => {
-  const userName = username === null ? user.email?.slice(0, user.email.indexOf("@")) : username;
-  // 檢查 userName 是否已存在
-  const usersRef = collection(db, "users");
-  const q = query(usersRef, where("userName", "==", userName));
-  const querySnapshot = await getDocs(q);
+const writeUser = async (
+  user: User,
+  username: string | null,
+  source: string,
+) => {
+  try {
+    const userName = username === null ? user.email?.slice(0, user.email.indexOf("@")) : username;
+    // 檢查 userName 是否已存在
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("userName", "==", userName));
+    const querySnapshot = await getDocs(q);
 
-  let userAccount = userName;
-  if (!querySnapshot.empty) {
-    // 如果用戶名已存在，再加入3位數亂數
-    const randomNum = Math.floor(Math.random() * 900) + 100; // 生成100-999之間的隨機數
-    userAccount = `${userName}_${randomNum}`;
+    let userAccount = userName;
+    if (!querySnapshot.empty) {
+      // 如果用戶名已存在，再加入3位數亂數
+      const randomNum = Math.floor(Math.random() * 900) + 100; // 生成100-999之間的隨機數
+      userAccount = `${userName}_${randomNum}`;
+    }
+
+    await setDoc(doc(db, "users", user.uid), {
+      uid: user.uid,
+      email: user.email,
+      userName,
+      userAccount,
+      createdAt: new Date(),
+      loginType: source,
+      userType: "0", // 0: 一般使用者, 1: 管理員
+    });
+    await setDoc(doc(db, "userSettings", user.uid), {
+      uid: user.uid,
+      avatarUrl: "",
+      bgColor: getRandomColor(),
+      darkMode: "dark",
+      toastifyPosition: "bottom-right",
+      template: "default",
+    });
+
+    return 'success';
+  } catch (error) {
+    return error;
   }
-
-  await setDoc(doc(db, "users", user.uid), {
-    uid: user.uid,
-    email: user.email,
-    userName,
-    userAccount,
-    createdAt: new Date(),
-    loginType: source,
-    userType: '0', // 0: 一般使用者, 1: 管理員
-  });
-  await setDoc(doc(db, "userSettings", user.uid), {
-    uid: user.uid,
-    avatarUrl: '',
-    bgColor: getRandomColor(),
-    darkMode: "dark",
-    toastifyPosition: "bottom-right",
-    template: "default",
-  });
 };
 
 /** 一般註冊 (Email+Password) */
@@ -86,14 +104,13 @@ export const loginWithEmailAndPassword = async (
       password,
     );
     const { user } = userCredential;
-    console.log(userCredential);
 
     const userData: userDataType = {
       id: user.uid,
       uid: user.uid,
       email: user.email,
-      displayName: get(user, "providerData[0].displayName", ''),
-      photoURL: get(user, "providerData[0].photoURL", ''),
+      displayName: get(user, "providerData[0].displayName", ""),
+      photoURL: get(user, "providerData[0].photoURL", ""),
       loginType: "email",
       createdAt: user.metadata.creationTime,
       emailVerified: user.emailVerified,
